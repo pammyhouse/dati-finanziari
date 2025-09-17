5fe1cdbb0153965c2dbe131cab8036c0671da9fc
pammyhouse <144261349+pammyhouse@users.noreply.github.com>
2025-09-17 22:19:56 +0200

    Update convert_gptneo.py

--- scripts/convert_gptneo.py
+++ scripts/convert_gptneo.py
@@ -1,24 +1,28 @@
-from transformers import GPTNeoForCausalLM, GPT2Tokenizer
+from transformers import TFGPTNeoForCausalLM, GPT2Tokenizer
 import tensorflow as tf
 
-# Scarica modello GPT-Neo 125M
+# Modello e tokenizer
 model_name = "EleutherAI/gpt-neo-125M"
-model = GPTNeoForCausalLM.from_pretrained(model_name)
-tokenizer = GPT2Tokenizer.from_pretrained(model_name)
 
-# Salva tokenizer localmente
+# Scarica tokenizer
+tokenizer = GPT2Tokenizer.from_pretrained(model_name)
 tokenizer.save_pretrained("./model_tf/tokenizer")
 
-# Converti in TensorFlow SavedModel
+# Scarica modello TF da PyTorch
+model = TFGPTNeoForCausalLM.from_pretrained(model_name, from_pt=True)
+
+# Salva come TensorFlow SavedModel
 model.save_pretrained("./model_tf/tf_model", saved_model=True)
 
-# Conversione in TFLite
+# Conversione in TFLite con quantizzazione FP16
 converter = tf.lite.TFLiteConverter.from_saved_model("./model_tf/tf_model")
 converter.optimizations = [tf.lite.Optimize.DEFAULT]
+converter.target_spec.supported_types = [tf.float16]  # FP16
+
 tflite_model = converter.convert()
 
 # Salva modello TFLite
-with open("./model_tf/gpt_neo_125.tflite", "wb") as f:
+with open("./model_tf/gpt_neo_125_fp16.tflite", "wb") as f:
     f.write(tflite_model)
 
-print("Conversione completata! File TFLite salvato in ./model_tf/gpt_neo_125.tflite")
+print("Conversione completata! File TFLite salvato in ./model_tf/gpt_neo_125_fp16.tflite")
